3d04526a9a29c9023db27ea49df5ac2793230dcc
chipush1lo <kkuygrttyyy@gmail.com>
2025-10-16 16:20:38 +0300

    Add files via upload

---
+++ TDDproject/ArithmeticProgression.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace TDDproject
+{
+    public class ArithmeticProgression : IArithmeticProgression
+    {
+        public double InitialElement { get; set; }
+        public double Difference { get; set; }
+
+        public ArithmeticProgression()
+        {
+            InitialElement = 0;
+            Difference = 1;
+        }
+
+        public ArithmeticProgression(double initialElement, double difference)
+        {
+            InitialElement = initialElement;
+            Difference = difference;
+        }
+
+        public double GetElement(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), "The element number must be positive.");
+            }
+            return InitialElement + (n - 1) * Difference;
+        }
+
+        public double GetSum(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), "The number of elements cannot be negative.");
+            }
+            if (n == 0)
+            {
+                return 0;
+            }
+            return (2 * InitialElement + (n - 1) * Difference) * n / 2.0;
+        }
+
+        public IArithmeticProgression Add(IArithmeticProgression other)
+        {
+            return new ArithmeticProgression(this.InitialElement + other.InitialElement, this.Difference + other.Difference);
+        }
+
+        public IArithmeticProgression Subtract(IArithmeticProgression other)
+        {
+            return new ArithmeticProgression(this.InitialElement - other.InitialElement, this.Difference - other.Difference);
+        }
+
+        public IArithmeticProgression Multiply(double number)
+        {
+            return new ArithmeticProgression(this.InitialElement * number, this.Difference * number);
+        }
+
+        public IArithmeticProgression Add(double number)
+        {
+            return new ArithmeticProgression(this.InitialElement + number, this.Difference);
+        }
+
+        public override string ToString()
+        {
+            return $"Progression: InitialElement = {InitialElement}, Difference = {Difference}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            ArithmeticProgression other = (ArithmeticProgression)obj;
+            return (InitialElement == other.InitialElement) && (Difference == other.Difference);
+        }
+
+        public override int GetHashCode()
+        {
+            return InitialElement.GetHashCode() ^ Difference.GetHashCode();
+        }
+    }
+}
---
+++ TDDproject/IArithmeticProgression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TDDproject
+{
+    public interface IArithmeticProgression
+    {
+        double InitialElement { get; set; }
+        double Difference { get; set; }
+
+        double GetElement(int n);
+        double GetSum(int n);
+        IArithmeticProgression Add(IArithmeticProgression other);
+        IArithmeticProgression Subtract(IArithmeticProgression other);
+        IArithmeticProgression Multiply(double number);
+        IArithmeticProgression Add(double number);
+    }
+}
